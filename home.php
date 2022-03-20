e368629053ee1c7bd305975a7e2a8ad65aae203e
nickbevers <nick.bevers9@gmail.com>
2022-03-20 14:24:59 +0100

    feat: adds homepage with security check

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php 
+
+    include_once(__DIR__ . "/helpers/Security.help.php");
+    Security::onlyLoggedInUsers();
+
+
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dezine Home</title>
+</head>
+<body>
+    HOME
+</body>
+</html>
