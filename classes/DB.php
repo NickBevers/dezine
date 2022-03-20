788e13d5a9e2a6c7876d365d7d78aac97ed2f84f
nickbevers <nick.bevers9@gmail.com>
2022-03-20 14:11:44 +0100

    feat: adds db class to the project with getInstance function

---
+++ classes/DB.php
@@ -0,0 +1,16 @@
+<?php
+    abstract class DB {
+        private static $conn;
+
+        public static function getInstance(){
+            if(self::$conn != null){
+                return self::$conn;
+            } else{
+                $config = parse_ini_file("config/config.ini");
+                self::$conn = new PDO('mysql:host='. $config['db_host'] .';dbname=' . $config['db_name'], $config['db_user'], $config['db_password'] );
+                return self::$conn;
+            }
+        }
+
+        
+    }
