d4abaac8c2002632ea1af42637b388e352daf57c
SarahOorts <r0657122@student.thomasmore.be>
2022-03-22 10:25:25 +0100

    feat: created new file reset.php which checks email and sends resetmail to the user

---
+++ reset.php
@@ -0,0 +1,43 @@
+<?php
+  include_once(__DIR__ . "/helpers/CheckEmpty.help.php");
+  include_once(__DIR__ . "/classes/Reset.php");
+
+  if(!empty($_POST)) {
+    if(CheckEmpty::isNotEmpty($_POST['email'])){          
+        $emailId = $_POST['email'];
+
+        $reset = new Reset();
+        $reset->resetMail($emailId);
+    }
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset password</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+   <body>
+      <div class="container">
+          <div class="card">
+            <div class="card-header text-center">
+              Reset password
+            </div>
+            <div class="card-body">
+              <form action="" method="post">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input type="email" name="email" class="form-control" id="email">
+                </div>
+                <input type="submit" name="reset-token" class="btn btn-primary">
+              </form>
+            </div>
+          </div>
+      </div>
+ 
+   </body>
+</html>
