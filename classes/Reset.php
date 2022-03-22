df8ba5369164cb94918ef1c489df649d1c627c78
SarahOorts <r0657122@student.thomasmore.be>
2022-03-22 10:54:45 +0100

    fix: added getters and setters for email and token. updated other functions with getter and setter data changed resetLink function from static to norma function. Added a static function for resetPassword in reset_password.php

--- classes/Reset.php
+++ classes/Reset.php
@@ -1,18 +1,44 @@
 <?php
 
 include_once(__DIR__ . "/DB.php");
+include_once(__DIR__ . "/../helpers/Cleaner.help.php");
 
     class Reset{
+        private $email;
+        private $token;
 
-        public function resetMail($emailId){
+        public function setEmail($email){
+            $email = Cleaner::cleanInput($email);
+            $this->email = $email;
+            return $this;
+        }
+
+        public function getEmail(){
+            return $this->email;
+        }
+
+        public function setToken($token){
+            $token = Cleaner::cleanInput($token);
+            $this->token = $token;
+            return $this;
+        }
+
+        public function getToken(){
+            return $this->token;
+        }
+
+        const PASSWORD_MIN_LENGTH = 6;
+
+        public function resetMail(){
+            $email = $this->email;
             $conn = DB::getInstance();
             $statement = $conn->prepare("select * from users where email = :email");
-            $statement->bindValue("email", $emailId);
+            $statement->bindValue("email", $email);
             $statement->execute();
             $result = $statement->fetch();
 
             if ($result) {
-                $token = md5($emailId).rand(10, 9999);
+                $token = md5($email).rand(10, 9999);
                 $expFormat = mktime(
                     date("H"),
                     date("i"),
@@ -27,12 +53,12 @@ include_once(__DIR__ . "/DB.php");
                 $state = $conn->prepare("update users set reset_token = :reset_token, exp_token = :exp_token where email = :email");
                 $state->bindValue("reset_token", $token);
                 $state->bindValue("exp_token", $expDate);
-                $state->bindValue("email", $emailId);
+                $state->bindValue("email", $email);
         
                 //link nog aanpassen
-                $link = "<a href='www.yourwebsite.com/reset-password.php?key=".$emailId."&token=".$token."'>Click To Reset password</a>";
+                $link = "<a href='www.yourwebsite.com/reset-password.php?key=".$email."&token=".$token."'>Click To Reset password</a>";
 
-                $to = $emailId;
+                $to = $email;
                 $subject = `Reset your Dezine Password!`;
                 $message = `It seems like you forgot your password, be sure to log back in again! \n Click here $link`;
 
@@ -41,9 +67,9 @@ include_once(__DIR__ . "/DB.php");
             }
         }
 
-        public static function resetLink($email, $token){
-            $em = $email;
-            $tok = $token;
+        public function resetLink(){
+            $em = $this->email;
+            $tok = $this->token;
             $conn = DB::getInstance();
             $query = $conn->prepare("select * from users where reset_token = :reset_token and email = :email");
             $query->bindValue("reset_token", $tok);
@@ -60,6 +86,24 @@ include_once(__DIR__ . "/DB.php");
             } else{
                 $message = "This forget password link has been expired";
                 return $message;
+            }
+        }
+
+        public static function resetPassword($email, $new_password){
+            if(strlen($new_password) < self::PASSWORD_MIN_LENGTH){
+                throw new Exception("Passwords must be " . self::PASSWORD_MIN_LENGTH . " characters or longer.");
+            } else{
+                $options = [
+                'cost' => 15
+                ];
+                $n_password = password_hash($new_password, PASSWORD_DEFAULT, $options);
+                $conn = DB::getInstance();
+                $statement = $conn->prepare("update users set password= :password where email= :email");
+                $statement->bindValue(':password', $n_password);
+                $statement->bindValue(':email', $email);
+                $statement->execute();
+                // $result = $statement->fetch();
+                // var_dump($result);
             }
         }
     }
