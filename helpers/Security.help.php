d039c2d592fa61945f1ef7ce953f3c6f661970b3
Elien De Geetere <elien.degeetere@gmail.com>
2022-04-01 12:45:55 +0200

    fix: removed comments

--- helpers/Security.help.php
+++ helpers/Security.help.php
@@ -1,18 +1,5 @@
 <?php
     abstract class Security {
-       /* public static function onlyLoggedInUsers() {
-            session_start();
-            if(!isset($_SESSION['email'])){
-                header("Location: login.php");
-            }
-        }
-
-        public static function alreadyLoggedIn(){
-            session_start();
-            if(isset($_SESSION['email'])){
-                header("Location: home.php");
-            }
-        }*/
 
         public static function isLoggedIn(){
             session_start();
