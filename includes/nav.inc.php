1fc907b924da95393954bb7eb47acab40f5ff3ef
SarahOorts <r0657122@student.thomasmore.be>
2022-04-05 20:15:18 +0200

    fix: added link home in nav

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="feed.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="new_post.php">New Post</a>
