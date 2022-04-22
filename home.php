8092fc02d7e0343b261e666fa8e3402f8e7e3335
SarahOorts <r0657122@student.thomasmore.be>
2022-04-22 10:53:54 +0200

    fix: add minimal nav

--- home.php
+++ home.php
@@ -27,9 +27,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>Dezine Home</title>
 </head>
 <body>
+<?php include_once(__DIR__ . "/includes/nav.inc.php"); ?>
     HOME
     <?php if(isset($_SESSION['email'])): ?>
         <h3> Welcome <?php echo $_SESSION['email'] ?></h3>
