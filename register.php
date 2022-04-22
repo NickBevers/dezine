fdd8af004b332df17b9c2d6d7004e423c92cf055
nickbevers <nick.bevers9@gmail.com>
2022-04-22 08:33:10 +0200

    feat: started css for register;

--- register.php
+++ register.php
@@ -40,8 +40,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Dezine</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
 </head>
 <body class="container">
 <?php include_once(__DIR__ . "/includes/nav.inc.php"); ?>
@@ -51,24 +52,24 @@
     <div class="alert alert-danger"><?php echo $error; ?></div>
 <?php endif; ?>
 
-<form method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>>
-  <div class="mb-3">
+<form method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> class="form form--register">
+  <div class="form__field">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
   </div>
 
-  <div class="mb-3">
+  <div class="form__field">
     <label for="exampleInputUsername1" class="form-label">Username</label>
-    <input type="text" name="username" class="form-control" id="exampleInputUsername1" required>
+    <input type="text" name="userngame" class="form-control" id="exampleInputUsername1" required>
   </div>
 
-  <div class="mb-3">
+  <div class="form__field">
     <label for="exampleInputPassword1" class="form-label">Password</label>
     <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
     <div id="passwordHelp" class="form-text">Passwords must be at least 6 characters long</div>
   </div>
 
-  <div class="mb-3">
+  <div class="form__field">
     <label for="password_conf" class="form-label">Password confirmation</label>
     <input type="password" name="password_conf" class="form-control" id="password_conf" required>
     <div id="passwordHelp" class="form-text">Passwords must match password above</div>
@@ -80,6 +81,6 @@
 
 </main>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> -->
 </body>
 </html>
