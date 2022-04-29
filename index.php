95e68918d34b119e0856bad1a7444f4783e36094
SarahOorts <r0657122@student.thomasmore.be>
2022-04-29 17:35:27 +0200

    feat: add grid gallery for unregistered visitors to index.php, changed layout slightly from grid gallery on the homefeed

--- index.php
+++ index.php
@@ -17,11 +17,31 @@
     <section class="bkg">
         <img src="./assets/imd_dezine.svg" alt="Dezine logo">
     </section>
+
     <section class="banner">
         <div class="banner__loop">
             <h1 class="banner__loop__text">&nbsp; Showcase of our beautiful work - Showcase of our beautiful work</h1>
             <h1 class="banner__loop__text2">&nbsp; Showcase of our beautiful work - Showcase of our beautiful work</h1>
         </div>
+    </section>
+
+    <section class="posts">
+    <?php $posts = POST::getSomePosts(0, 6); ?>
+    <?php foreach($posts as $post): ?>
+        <div class="post post--index">
+            <img src=<?php echo $post["image"] ?> alt=<?php echo $post["title"] ?>>
+            <div class="post__info">
+                <h3><?php echo $post["title"] ?></h3>
+                <p><?php echo $post["description"] ?></p>
+                <?php $tags = json_decode($post["tags"]); ?>
+                <div class="post__info__tags">
+                    <?php foreach($tags as $t): ?>
+                        <p><?php echo "#"; echo $t; echo "&nbsp"; ?></p>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>              
+    <?php endforeach; ?>
     </section>
     
     <?php include_once("./includes/footer.inc.php"); ?>
