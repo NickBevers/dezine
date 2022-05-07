d11958704ac8d5f5a515aa24e1d1d3a9a4cc0ae0
SarahOorts <r0657122@student.thomasmore.be>
2022-05-07 15:27:43 +0200

    feat: add small text and button for adding or removing bann and function check if user is already banned. Added js link for ajax banning or removing ban

--- moderator_overview.php
+++ moderator_overview.php
@@ -9,6 +9,9 @@
         header('Location: home.php');
     }
 
+    $banId = Cleaner::cleanInput($_GET["id"]);
+    $user = User::getUserbyId($banId);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,9 +21,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.typekit.net/nkx6euf.css">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Moderator overview</title>
+    <title>Moderator Overviewpage</title>
 </head>
 <body>
+    <?php include_once(__DIR__ . "/includes/nav.inc.php"); ?>
     <h1>Moderator Overviewpage</h1>
+
+    <div class="alert alert-success hidden"></div>
+
+    <?php if(User::checkBan($banId)): ?>
+        <div class="banning hidden">
+            <h2>Would you like to ban user <?php echo $user["username"]; ?>?</h2>
+            <button href="#" class="btn btn-primary ban" data-id="<?php echo $banId; ?>">Ban user</button>
+        </div>
+        <div class="banned">
+            <h2>Would you like to retract the ban against user <?php echo $user["username"]; ?>?</h2>
+            <button href="#"class="btn btn-primary unban" data-id="<?php echo $banId; ?>">Retract Ban</button>
+        </div>        
+    <?php else: ?>    
+        <div class="banning">
+            <h2>Would you like to ban user <?php echo $user["username"]; ?>?</h2>
+            <button href="#" class="btn btn-primary ban" data-id="<?php echo $banId; ?>">Ban user</button>
+        </div>
+        <div class="banned hidden">
+            <h2>Would you like to retract the ban against user <?php echo $user["username"]; ?>?</h2>
+            <button href="#"class="btn btn-primary unban" data-id="<?php echo $banId; ?>">Retract Ban</button>
+        </div>  
+    <?php endif; ?>
+
+    <script src="./javascript/add_remove_ban.js"></script>
 </body>
 </html>
