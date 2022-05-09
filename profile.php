b4860434c6473263a20f8181f0c2f3dabf15c1d5
Cournflaeik <elien.degeetere@gmail.com>
2022-05-09 15:39:09 +0200

    test: Nick's issue

--- profile.php
+++ profile.php
@@ -79,7 +79,9 @@
                 <?php if($user["user_role"] !== "user" && User::checkUserRole($uid) !== "user"): ?>
                     <img src="assets\icon_check.svg" class="profile__info__details__verified" alt="verified icon">    
                 <?php endif; ?> 
-                <?php if($user["id"] !== intval($uid) && User::checkUserRole($uid) === "admin"): ?>
+                <?php if(intval($user["id"]) !== intval($uid) && User::checkUserRole($uid) === "admin"): ?> 
+                    <!-- intval around $user["id"] is not supposed to be there, we'll figure it out soon, Nick's problem (Nick approved) -->
+                    <?php var_dump($uid); var_dump($user["id"]); ?>
                     <form action="#" method="post">
                         <?php if($user["user_role"] === "user"): ?>
                             <button name="moderator" value="assign" type="submit">Make moderator</button>
