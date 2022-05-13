be093c357496e00822c2105db601192a22cae32e
SarahOorts <r0657122@student.thomasmore.be>
2022-05-13 13:49:06 +0200

    feat: add bootstrap + namespaces to ajax files follow-unfollow, ban and addtoshowcase

--- ajax/addToShowcase.php
+++ ajax/addToShowcase.php
@@ -4,29 +4,20 @@
 
     if (!empty($_POST)) {
         $postId = $_POST['postId'];
+        $userId = $_POST["userId"];
     
         try {
-            $showcase = new \Classes\Content\Showcase();
+            $showcase = new Showcase();
             $showcase->setPostId($postId);
-            var_dump($showcase->setPostId($postId));
-            $showcase->setUserId($_SESSION["id"]);
-            var_dump("does work");
-            if(Showcase::checkShowcase($postId, $_SESSION["id"])){
-                var_dump($showcase);
-                var_dump("does work 1");
+            $showcase->setUserId($userId);
+            if(Showcase::checkShowcase($postId, $userId)){
                 $showcase->removeFromShowcase();
-                var_dump($showcase->removeFromShowcase());
-                var_dump("does work 2");
                 $response = [
                     "status" => "success",
                     "message" => "Remove from showcase was successfull."
                 ];
             }else{
-                var_dump($showcase);
-                var_dump("does work 3");
                 $showcase->addToShowcase();
-                var_dump($showcase->addToShowcase());
-                var_dump("does work 4");
                 $response = [
                     "status" => "success",
                     "message" => "Add to showcase was successfull."
