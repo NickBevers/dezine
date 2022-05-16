2ac600ec1a8aa1ce1339c8183535badc5c0dfdd2
nickbevers <nick.bevers9@gmail.com>
2022-05-16 19:39:47 +0200

    fix: makes the code for banning easier;

--- moderator.php
+++ moderator.php
@@ -2,14 +2,16 @@
     include_once(__DIR__ . "/autoloader.php");
     include_once("./helpers/Cleaner.help.php");
     include_once("./helpers/Security.help.php");
-    if(!Security::isLoggedIn()) { header('Location: login.php');}
+    if (!Security::isLoggedIn()) {
+        header('Location: login.php');
+    }
 
     $uid = Cleaner::cleanInput($_SESSION["id"]);
-    if(!User::checkModerator($uid)){
+    if (!User::checkModerator($uid)) {
         header('Location: home.php');
     }
 
-    if(isset($_GET["id"])){
+    if (isset($_GET["id"])) {
         $banId = Cleaner::cleanInput($_GET["id"]);
         $user = User::getUserbyId($banId);
     }
@@ -33,41 +35,29 @@
     <main>
         <h1>Moderator Overviewpage</h1>
         <!-- styling nog doen -->
-        <?php if(isset($_GET["id"])): ?>
+        <?php if (isset($_GET["id"])): ?>
         <div class="alert alert-success hidden"></div>
-
-        <?php if(User::checkBan($banId)): ?>
-            <div class="banning hidden">
-                <h2>Would you like to ban user <?php echo $user["username"]; ?>?</h2>
-                <button href="#" class="btn secondary__btn secondary__btn-signup ban" data-id="<?php echo $banId; ?>">Ban user</button>
-            </div>
-            <div class="banned">
-                <h2>Would you like to retract the ban against user <?php echo $user["username"]; ?>?</h2>
-                <button href="#"class="btn secondary__btn secondary__btn-signup unban" data-id="<?php echo $banId; ?>">Retract Ban</button>
-            </div>        
-        <?php else: ?>    
-            <div class="banning">
-                <h2>Would you like to ban user <?php echo $user["username"]; ?>?</h2>
-                <button href="#" class="btn secondary__btn secondary__btn-signup ban" data-id="<?php echo $banId; ?>">Ban user</button>
-            </div>
-            <div class="banned hidden">
-                <h2>Would you like to retract the ban against user <?php echo $user["username"]; ?>?</h2>
-                <button href="#"class="btn secondary__btn secondary__btn-signup unban" data-id="<?php echo $banId; ?>">Retract Ban</button>
-            </div>  
-        <?php endif; ?>
+        <div class="banning <?php if (User::checkBan($banId)) { echo "hidden"; } ?>">
+            <h2>Would you like to ban user <?php echo $user["username"]; ?>?</h2>
+            <button href="#" class="btn secondary__btn secondary__btn-signup ban" data-id="<?php echo $banId; ?>">Ban user</button>
+        </div>
+        <div class="banned <?php if (!User::checkBan($banId)) { echo "hidden"; } ?>">
+            <h2>Would you like to retract the ban against user <?php echo $user["username"]; ?>?</h2>
+            <button href="#"class="btn secondary__btn secondary__btn-signup unban" data-id="<?php echo $banId; ?>">Retract Ban</button>
+        </div>
         <script src="./javascript/add_remove_ban.js"></script>
         <?php endif; ?>
-        <?php if(!isset($_GET["id"])): ?>
+        <?php if (!isset($_GET["id"])): ?>
         <div class="reports form form--profile"> 
-            <?php foreach($reports as $report): ?>
-                <?php if(intval($report["archived"]) == 0): ?>
+            <?php foreach ($reports as $report): ?>
+                <?php if (intval($report["archived"]) == 0): ?>
                     <div class="report">
                     <?php $post = Post::getPostbyPostId($report["post_id"]); ?>
-                        <?php if(intval($report["post_id"]) !== 0): ?>
+                        <?php if (intval($report["post_id"]) !== 0): ?>
                             <a href="detailsPost.php?pid=<?php echo $post["id"];?>">
                                 <img src="<?php echo $post["image"]; ?>" class="reports__post__img" alt="reported post">
                             </a>
-                        <?php elseif(intval($report["post_id"]) == 0): ?>
+                        <?php elseif (intval($report["post_id"]) == 0): ?>
                             <img src="<?php echo(User::getProfileImagebyId($report["user_id"])["profile_image"]); ?>" class="reports__user__img" alt="profile picture <?php echo($report["user_id"]); ?>">
                             <p class="reports__user__username"> Username: 
                                 <a href="profile.php?pid=<?php echo $report["user_id"];?>">
