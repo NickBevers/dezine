84129d31b8114d984183b31ab70c47dc24703f93
nickbevers <nick.bevers9@gmail.com>
2022-05-17 18:46:46 +0200

    fix: simplify the api/feed;

--- api/feed.php
+++ api/feed.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Posts</title>
-    <style>
-        body{
-            background-color: hsl(0, 0%, 12%);
-            color: aliceblue;
-        }
-    </style>
-</head>
-<body>
-    
-<script src="./../javascript/getApi.js"></script>
-</body>
-</html>
+<?php 
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json;charset=utf-8');
+    include_once("./../classes/Post.php");
+    $posts = Post::getSomePosts("desc", 0, 40);
+    echo json_encode($posts);
+?>
