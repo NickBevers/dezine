e230313ac643bdc214c7d3b03ec15b23b18af1ed
SarahOorts <r0657122@student.thomasmore.be>
2022-05-27 14:46:00 +0200

    fix: add if else to footer for different layout on index.php

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,22 +1,45 @@
-<footer>
-    <div class="footer__info">
-        <img src="./assets/TM.svg" alt="Thomas More logo">
-        <p>The platform d-zine was made by second year Interactive and Multimedia Design students at Thomas More University of Applied Sciences.</p>
-    </div>
-    <div class="footer__socials">
-        <a href="mailto:" target="_blank">
-            <img src="./assets/mail_icon.svg" alt="email icon">
-        </a>
-        <a href="https://www.facebook.com/WeAreIMD/" target="_blank">
-            <img src="./assets/fb_icon.svg" alt="facebook icon">
-        </a>
-        <a href="https://www.instagram.com/imdvibes/?hl=en" target="_blank">
-            <img src="./assets/insta_icon.svg" alt="instagram icon">
-        </a>
-        <a href="https://weareimd.be/" target="_blank">
-            <img src="./assets/web_icon.svg" alt="website icon">
-        </a>
-    </div>
-</footer>
+<?php if ($_SERVER['REQUEST_URI'] !== "/dezine/index.php"): ?>
+    <footer>
+        <div class="footer__info">
+            <img src="./assets/TM.svg" alt="Thomas More logo">
+            <p>The platform d-zine was made by second year Interactive and Multimedia Design students at Thomas More University of Applied Sciences.</p>
+        </div>
+        <div class="footer__socials">
+            <a href="mailto:" target="_blank">
+                <img src="./assets/mail_icon.svg" alt="email icon">
+            </a>
+            <a href="https://www.facebook.com/WeAreIMD/" target="_blank">
+                <img src="./assets/fb_icon.svg" alt="facebook icon">
+            </a>
+            <a href="https://www.instagram.com/imdvibes/?hl=en" target="_blank">
+                <img src="./assets/insta_icon.svg" alt="instagram icon">
+            </a>
+            <a href="https://weareimd.be/" target="_blank">
+                <img src="./assets/web_icon.svg" alt="website icon">
+            </a>
+        </div>
+    </footer>
+<?php else: ?>
+    <footer class="footer--index">
+        <div class="footer__info">
+            <img src="./assets/TM.svg" alt="Thomas More logo">
+            <p>The platform d-zine was made by second year Interactive and Multimedia Design students at Thomas More University of Applied Sciences.</p>
+        </div>
+        <div class="footer__socials">
+            <a href="mailto:" target="_blank">
+                <img src="./assets/mail_icon.svg" alt="email icon">
+            </a>
+            <a href="https://www.facebook.com/WeAreIMD/" target="_blank">
+                <img src="./assets/fb_icon.svg" alt="facebook icon">
+            </a>
+            <a href="https://www.instagram.com/imdvibes/?hl=en" target="_blank">
+                <img src="./assets/insta_icon.svg" alt="instagram icon">
+            </a>
+            <a href="https://weareimd.be/" target="_blank">
+                <img src="./assets/web_icon.svg" alt="website icon">
+            </a>
+        </div>
+    </footer>
+<?php endif; ?>
 
 
