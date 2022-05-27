208633dbe7e7430150d25f8fe4690e204a0a6666
SarahOorts <r0657122@student.thomasmore.be>
2022-05-27 21:50:16 +0200

    fix: cleaned up code plus add previous code for functionality

--- javascript/add_remove_ban.js
+++ javascript/add_remove_ban.js
@@ -7,6 +7,7 @@ document.querySelector(".ban").addEventListener("click", e => {
 document.querySelector(".unban").addEventListener("click", e => {
     addRemoveBan(e);
 });
+
 function addRemoveBan(e) {
     // console.log("clicked");
     let data = new FormData();
@@ -16,26 +17,26 @@ function addRemoveBan(e) {
     fetch("ajax/ban.php", {
             method: "POST",
             body: data,
-        })
-        .then(response => response.json())
-        .then(res => {
-            if (res.status === "success") {
-                // console.log("Success: " + res.message);
-                if (res.message === "The ban has been lifted") {
-                    document.querySelector(".banning").classList.remove("hidden");
-                    document.querySelector(".banned").classList.add("hidden");
-                } else if (res.message === "User has been banned") {
-                    document.querySelector(".banned").classList.remove("hidden");
-                    document.querySelector(".banning").classList.add("hidden");
-                }
-                document.querySelector(".alert").classList.remove("hidden");
-                document.querySelector(".alert").innerHTML = res.message;
-            } else {
-                console.error(`Something has gone wrong: ${res.message}`)
+    })
+    .then(response => response.json())
+    .then(res => {
+        if (res.status === "success") {
+            // console.log("Success: " + res.message);
+            if (res.message === "The ban has been lifted") {
+                document.querySelector(".banning").classList.remove("hidden");
+                document.querySelector(".banned").classList.add("hidden");
+            } else if (res.message === "User has been banned") {
+                document.querySelector(".banned").classList.remove("hidden");
+                document.querySelector(".banning").classList.add("hidden");
             }
-        })
-        .catch((error) => {
-            console.error("Error: ", error);
-        });
+            document.querySelector(".alert").classList.remove("hidden");
+            document.querySelector(".alert").innerHTML = res.message;
+        } else {
+            console.error(`Something has gone wrong: ${res.message}`)
+        }
+    })
+    .catch((error) => {
+        console.error("Error: ", error);
+    });
     e.preventDefault();
 }
